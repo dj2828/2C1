519a7b7bfb62039f6298e6f0be4f723573a75013
dj 2828 <dj28partystudio@gmail.com>
2025-04-04 20:08:56 +0200

    fix dalla profe

--- cpp/laboratorio/while/es 3 altra scheda.cpp
+++ cpp/laboratorio/while/es 3 altra scheda.cpp
@@ -2,22 +2,24 @@
 using namespace std;
 
 int main() {
-    int n, maxEta = -1, minEta = 101, sommaEta = 0, maggiorenni = 0, bambini = 0, i = 0;
+    int eta, n, maxEta, minEta, sommaEta = 0, maggiorenni = 0, bambini = 0, i = 0;
     float etaMedia;
 
     cout << "Inserisci il numero di persone: ";
     cin >> n;
 
     while (i < n) {
-        int eta;
         cout << "Inserisci l'eta di una persona ";
         cin >> eta;
 
-        // Calcolo dell'età massima e minima
-        if (eta > maxEta){
+        if (i == 0) {
+            maxEta = eta;
+            minEta = eta;
+        }
+        else if (eta > maxEta) {
             maxEta = eta;
         }
-        if (eta < minEta){
+        else if (eta < minEta) {
             minEta = eta;
         }
 
